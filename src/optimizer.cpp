94f847e29cdec3b3e8cf4435a9e9d93ef3bea823
jkim <jkim@Sanyomachine.ca>
2026-08-21 16:28:53 -0400

    propagator complete

--- src/optimizer.cpp
+++ src/optimizer.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <vector>
 #include <memory>
+#include <unordered_map>
 
 
 void Optimizer::optimize(){
@@ -93,5 +94,115 @@ bool Optimizer::folder(IRFunction& func){
     return folded;
 }
 
+bool Optimizer::propagator(IRFunction& func){
 
+    //map key is var/temp name, and IRValue is replacement value
+    std::unordered_map<std::string, IRValue> knownValues;
+    bool changed = false;
+    int i = 0;
+
+    for(auto& instruct : func.instructions){
+        if(auto des = dynamic_cast<IRMove*>(instruct.get())){
+            //first propagate
+            if(des->source.kind == IRValueKind::Variable || des->source.kind == IRValueKind::Temporary){
+                auto it = knownValues.find(des->source.name);
+
+                if(it != knownValues.end()){
+                    des->source = (*it).second; //propagate source with replacement
+                    changed = true; //propagation modifies, so changed = true
+                }
+            }
+            knownValues[des->destination.name] = des->source; //update destination with source
+            
+            for(auto it = knownValues.begin(); it!=knownValues.end(); ){
+                if((it->second.kind == IRValueKind::Variable || it->second.kind == IRValueKind::Temporary) && it->second.name == des->destination.name) it = knownValues.erase(it);
+                else ++i;
+            }
+            
+        }
+        
+        else if(auto* des = dynamic_cast<IRConst*>(instruct.get())){
+            knownValues[des->destination.name] = des->value;
+            //no changed = true needed, only updating map, not modifying any instructions
+        }
+        else if(auto des = dynamic_cast<IRBinOp*>(instruct.get())){
+            if(des->leftVal.kind == IRValueKind::Variable || des->leftVal.kind == IRValueKind::Temporary){
+                auto it = knownValues.find(des->leftVal.name);
+                if(it != knownValues.end()){
+                    des->leftVal = (*it).second;
+                    changed = true;
+                }
+            }
+            if(des->rightVal.kind == IRValueKind::Variable || des->rightVal.kind == IRValueKind::Temporary){
+                auto it = knownValues.find(des->rightVal.name);
+                if(it != knownValues.end()){
+                    des->rightVal = (*it).second;
+                    changed = true;
+                }
+            }
+
+            for(auto it = knownValues.begin(); it!=knownValues.end(); ){
+                if((it->second.kind == IRValueKind::Variable || it->second.kind == IRValueKind::Temporary) && it->second.name == des->destination.name) it = knownValues.erase(it);
+                else ++it;
+            }
+            knownValues.erase(des->destination.name);
+            //dont update destination.name with bin result yet
+            //save it for later when we constant fold the IRBinOp into a IRConst
+        }
+        else if(auto des = dynamic_cast<IRUnaryOpStruct*>(instruct.get())){
+            if(des->value.kind == IRValueKind::Variable || des->value.kind == IRValueKind::Temporary){
+                auto it = knownValues.find(des->value.name);
+                if(it != knownValues.end()){
+                    des->value = (*it).second;
+                    changed = true;
+                }                
+            }
+
+            for(auto it = knownValues.begin(); it!=knownValues.end(); ){
+                if((it->second.kind == IRValueKind::Variable || it->second.kind == IRValueKind::Temporary) && it->second.name == des->destination.name) it = knownValues.erase(it);
+                else ++it;
+            }
+            knownValues.erase(des->destination.name);
+        }
+        else if(auto des = dynamic_cast<IRCall*>(instruct.get())){
+            for(auto& arg : des->arguments){
+                if(arg.kind == IRValueKind::Variable || arg.kind == IRValueKind::Temporary){
+                    auto it = knownValues.find(arg.name);
+                    if(it != knownValues.end()){
+                        arg = (*it).second;
+                        changed = true;
+                    }
+                }
+            }
+
+            for(auto it = knownValues.begin(); it!=knownValues.end(); ){
+                if((it->second.kind == IRValueKind::Variable || it->second.kind == IRValueKind::Temporary) && it->second.name == des->destination.name) it = knownValues.erase(it);
+                else ++it;
+            }
+            knownValues.erase(des->destination.name);
+        }
+        else if(auto des = dynamic_cast<IRReturn*>(instruct.get())){
+            if(des->value.kind == IRValueKind::Variable || des->value.kind == IRValueKind::Temporary){
+                auto it = knownValues.find(des->value.name);
+                if(it != knownValues.end()){
+                    des->value = (*it).second;
+                    changed = true;
+                }
+            }
+        }
+        else if(auto des = dynamic_cast<IRBranch*>(instruct.get())){
+            if(des->condition.kind == IRValueKind::Variable || des->condition.kind == IRValueKind::Temporary){
+                auto it = knownValues.find(des->condition.name);
+                if(it != knownValues.end()){
+                    des->condition = (*it).second;
+                    changed = true;
+                }
+            }
+            knownValues.clear();
+        }
+        else if(auto des = dynamic_cast<IRJump*>(instruct.get())) knownValues.clear();
+        else if(auto des = dynamic_cast<IRLabel*>(instruct.get())) knownValues.clear();
+
+    }
+}
 
